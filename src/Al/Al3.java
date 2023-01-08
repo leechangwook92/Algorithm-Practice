7010e18017b8a91f36deebdf8e91b0b69acae599
changwook lee <lcy12225@naver.com>
2023-01-08 23:09:47 +0900

    자연수 뒤집어 배열로 만들기

---
+++ src/Al/Al3.java
@@ -0,0 +1,25 @@
+package Al;
+
+
+public class Al3 {
+    public static void main(String[] args) {
+        Al3 a1 = new Al3();
+
+        int[] solution = a1.solution(12345);
+        for (int i : solution) {
+            System.out.println(i);
+        }
+
+    }
+
+    public int[] solution(long n) {
+        String[] arr = String.valueOf(n).split("");
+        int[] answer = new int[arr.length];
+
+        for(int i=0, j=answer.length-1; i<answer.length; i++, j--) {
+            answer[i] = Integer.valueOf(arr[j]);
+        }
+        return answer;
+    }
+
+}
