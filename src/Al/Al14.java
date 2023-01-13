7661bf97b1a4e2a73e6f4940d95f7e9da6d86f03
changwook lee <lcy12225@naver.com>
2023-01-14 04:24:25 +0900

    옷가게 할인 받기

---
+++ src/Al/Al14.java
@@ -0,0 +1,36 @@
+package Al;
+
+
+public class Al14 {
+    public static void main(String[] args) {
+
+        // 머쓱이네 옷가게는 10만 원 이상 사면 5%, 30만 원 이상 사면 10%, 50만 원 이상 사면 20%를 할인해줍니다.
+        // 구매한 옷의 가격 price가 주어질 때, 지불해야 할 금액을 return 하도록 solution 함수를 완성해보세요.
+
+
+        Al14 a1 = new Al14();
+
+        int solution = a1.solution(150000);
+        int solution2 = a1.solution(580000);
+
+        System.out.println(solution);
+        System.out.println(solution2);
+
+    }
+
+    public int solution(int price) {
+        int answer = 0;
+
+        if ( price >= 500000) {
+            answer = (int) (price*0.8);
+        } else if ( price >= 300000) {
+            answer = (int) (price*0.9);
+        } else if(price >= 100000) {
+            answer = (int) (price*0.95);
+        } else {
+            answer = price;
+        }
+
+        return answer;
+    }
+}
